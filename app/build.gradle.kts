05737a8b7b49f662325cf20538638a5ed7766bac
denisbgavin-code <denis.bgavin@gmail.com>
2026-09-19 17:52:56 +0300

    Bump Save As workflow to version 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.volt1.recorder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
     }
 
     buildTypes {

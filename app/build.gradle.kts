04141c67270f0e3da1e1b1cd152ca2521d98fbd2
denisbgavin-code <denis.bgavin@gmail.com>
2026-09-19 18:16:23 +0300

    Bump compact save picker release to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.volt1.recorder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
     }
 
     buildTypes {

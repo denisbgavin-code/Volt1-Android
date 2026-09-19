3ae1199aa566680ab6b904bde95cb8997ef46550
denisbgavin-code <denis.bgavin@gmail.com>
2026-09-19 18:45:10 +0300

    Bump gesture undo release to 0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.volt1.recorder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.3"
+        versionCode = 9
+        versionName = "0.4.4"
     }
 
     buildTypes {

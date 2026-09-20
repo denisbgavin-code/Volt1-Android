3dc6f1f2a8b3c448871d5c09f3aa9e75fb78af1b
denisbgavin-code <denis.bgavin@gmail.com>
2026-09-20 18:59:03 +0300

    Bump recording finalization fix to 0.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.volt1.recorder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.4.6"
+        versionCode = 12
+        versionName = "0.4.7"
     }
 
     buildTypes {
